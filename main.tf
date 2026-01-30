9f6be0956989afc25d6e34567dbf8617b3904170
MdMaaz2003 <maazmohd151@gmail.com>
2026-01-31 01:34:26 +0530

    provisioned more

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "react_ec2" {
+  count                  = var.instance_count
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.react_key.key_name
   vpc_security_group_ids = [aws_security_group.react_sg.id]
 
   tags = {
-    Name = "react-app-server"
+    Name = "react-app-server-${count.index}"
   }
 }
 
