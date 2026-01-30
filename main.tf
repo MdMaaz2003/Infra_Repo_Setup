a5da96b3ca467254af247d0bba89e56b9f488d6b
MdMaaz2003 <maazmohd151@gmail.com>
2026-01-31 00:10:03 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "react_ec2" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.react_key.key_name
   vpc_security_group_ids = [aws_security_group.react_sg.id]
-
 
   tags = {
     Name = "react-app-server"
--- variables.tf
+++ variables.tf
@@ -4,13 +4,6 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2"
-  type        = string
-  default     = "ami-0c02fb55956c7d316" # us-east-1
-
-}
-
 variable "key_name" {
   description = "EC2 Key Pair name"
   type        = string
